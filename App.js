b4d6901337f21c92070a036c8cc9a933b66cc0e2
Saniya-10 <143614094+Saniya-10@users.noreply.github.com>
2024-04-26 18:42:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'; // Don't forget to import React in each file where JSX is used
+import './App.css';
+import Loginpage from './Loginpage';
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+export default App;
